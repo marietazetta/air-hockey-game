6115f76f9fb5c04f57d0d26e7ca3680485b52278
Maria Ramirez <maria.ramirez.laguia@gmail.com>
2024-04-23 23:31:56 +0200

    no changes made

--- js/01_index.js
+++ js/01_index.js
@@ -1,16 +1,3 @@
 window.onload = () => Game.init() // A falta de botón, tenemos el inicializador directamente en cuanto abra la página.
 
 
-function showToast(message) {
-    console.log("showToast called!");
-
-    let toastCard = document.querySelector('#toast')
-    toastCard.classList.add("show")
-
-
-    const timeoutID = setTimeout(() => {
-        toastCard.classList.remove("show")
-
-    }, 3000)
-
-}
